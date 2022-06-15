8de494510fc8d85accb4ae7593be16abf75f3cd0
Miguel <Miguel>
2022-06-14 21:36:29 -0300

    primer comit desde vscode

---
+++ funciones.js
@@ -0,0 +1,15 @@
+const arreglo = [1,2,3];
+
+let[,,uno] = arreglo;
+
+console.log(uno);
+
+
+const programador ={
+    name: 'Michael',
+    age: 20
+}
+
+let {age} = programador; 
+
+console.log(age);
